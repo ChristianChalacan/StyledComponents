afc716b1e57bc10801f5a9a494a8c8c9e70df5d6
ChristianChalacan <christianchalacan@gmail.com>
2024-07-17 21:33:20 -0500

    Inicio del curso

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`;
+
+const Content = styled.div`
+  padding: 20px 25px;
+`;
+// Ejemplo de componente usando los props para dar un estilo determinado
+const Button = styled.button`
+  background-color: ${(props) => (props.primary ? "red" : "white")};
+  color: ${(props) => (props.primary ? "white" : "red")};
+  padding: 10px 15px;
+  border: solid 2px red;
+  border-radius: 4px;
+  margin-left: 5px;
+  margin-bottom: 10px;
+`;
+// Extendemos(Copiamos las propiedades) del componente Button
+// y creamos un nuevo componente con mas propiedades
+const BlockButton = styled(Button)`
+  width: 100%;
+  font-size: 24px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Content>
+      <P>Hola soy un parrafo</P>
+      <Button primary={true}>Enviar</Button>
+      <Button primary={false}>Enviar</Button>
+      <BlockButton primary>Enviar Grande</BlockButton>
+      <BlockButton>Enviar Grande</BlockButton>
+    </Content>
   );
 }
 
